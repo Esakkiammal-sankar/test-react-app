b1ee8ded39452f8891ec143de41edea132c62b6c
saranya <saranyalakshan01@gmail.com>
2026-03-17 21:22:28 +0530

    testing 01 branch

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// from testing-01 branch
 // https://vite.dev/config/jehruwer
 export default defineConfig({
   plugins: [react()],
